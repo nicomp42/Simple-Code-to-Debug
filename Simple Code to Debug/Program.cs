e0ac07edd17d9a5b96bf54d62496cd5834f44192
nicomp42 <nicholdw@ucmail.uc.edu>
2022-10-03 09:55:19 -0400

    Add project files.

---
+++ Simple Code to Debug/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Simple code to debug.
+ * There's a logic error!
+ * The output should be 77.83 but it's not
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ */
+
+int[] grades = { 100, 95, 95, 77, 100, 0 };
+double averageGrade;
+int total = 0;
+
+foreach (int grade in grades)
+{
+    total = total + grade;
+}
+Console.WriteLine(total);
+averageGrade = total / grades.Length;
+Console.WriteLine("Average Grade = " + averageGrade);  // Why isn't this 77.83?
